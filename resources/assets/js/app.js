0738acf7bcd0ca896a7167b5028d88b8f9fd9f97
TomasRay <tomasvaiciunas@live.com>
2024-11-01 15:56:48 +0200

    Fixed css layout regarding the gaps in the index page

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,22 @@
 import './bootstrap.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+function typeText(elementId, text, speed = 100) {
+    const animatedText = document.getElementById(elementId);
+    let index = 0;
+
+    function type() {
+        if (index < text.length) {
+            animatedText.innerHTML += text.charAt(index);
+            index++;
+            setTimeout(type, speed);
+        }
+    }
+
+    type();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    typeText("animated-text", "Welcome to Conference World!", 100);
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-<div class="welcome">
-    Welcome
-</div>
-
-
+    <div id="welcome-wrapper">
+        <div class="container text-center welcome">
+            <div id="animated-text"></div>
+        </div>
+    </div>
 @endsection
